d280dc6be05833960397e760d36d4226d1c50c12
Larry Wen <l.wen@griffith.edu.au>
2024-11-02 16:37:20 +1000

    add database

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,26 @@
-var express = require('express');
+// routes/users.js
+
+var express = require("express");
 var router = express.Router();
+const { Pool } = require("pg");
+
+// Use the same pool created in app.js
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", async function (req, res, next) {
+  try {
+    const result = await pool.query("SELECT * FROM user"); // Adjust table name if necessary
+    res.json(result.rows);
+  } catch (err) {
+    console.error("Error executing query", err.stack);
+    res.status(500).send("Server Error");
+  }
 });
 
 module.exports = router;
